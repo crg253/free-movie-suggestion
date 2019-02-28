fcf229bde5737d41dfe6d7ce80d1acc37796426b
crg253 <crg253@protonmail.com>
2019-02-28 14:24:50 -0800

    New file structure for backend. Now calling it flask_api. Created config file. Created Movie and Tag models and their many to many table. Initialized migrations folder and created db.

---
+++ flask_api/app/models.py
@@ -0,0 +1,20 @@
+from app import db
+
+#Many to many
+
+tags = db.Table('tags',
+    db.Column('movie_id', db.Integer, db.ForeignKey('movie.movie_id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.tag_id'), primary_key=True)
+)
+
+class Movie(db.Model):
+    movie_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    year = db.Column(db.Integer)
+    video_link = db.Column(db.String(1000))
+    tags = db.relationship('Tag', secondary=tags, lazy='subquery',
+        backref=db.backref('movies', lazy=True))
+
+class Tag(db.Model):
+    tag_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
