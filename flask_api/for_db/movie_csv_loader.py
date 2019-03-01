5a9c0d7b05971d718214e2df33fb3cb58bd207fc
crg253 <crg253@protonmail.com>
2019-02-28 18:28:28 -0800

    Added uniquename field to Movie model, migrated changes. Made changes to genre names in csv. Modified csv loader.

--- flask_api/for_db/movie_csv_loader.py
+++ flask_api/for_db/movie_csv_loader.py
@@ -1,5 +1,6 @@
 import csv
-from flask_app import Movie, db
+from ../app import db
+from ../app.models import Movie
 import json
 
 def csvLoader(file):
@@ -14,10 +15,9 @@ def csvLoader(file):
                                     name=row[0],
                                     year=row[1],
                                     video_link=row[2],
-                                    genre=row[3])
                 db.session.add(uniquename)
                 db.session.commit()
 
     for movie in Movie.query.all():
-        print(str(movie.id) + ' ' + movie.uniquename + ' ' + movie.name + ' ' + str(movie.year) + ' ' + movie.video_link + ' ' + movie.genre )
+        print(str(movie.movie_id) + ' ' + movie.uniquename + ' ' + movie.name + ' ' + str(movie.year) + ' ' + movie.video_link)
         print(json.dumps({'uniquename': movie.uniquename}))
