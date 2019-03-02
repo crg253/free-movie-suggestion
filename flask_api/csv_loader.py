43357ca380556c0732bc209675bc7cbb5adb4dfd
crg253 <crg253@protonmail.com>
2019-03-01 18:56:07 -0800

    Removed spaces from csv file, and impoved csv_loader.

--- flask_api/csv_loader.py
+++ flask_api/csv_loader.py
@@ -21,40 +21,43 @@ def CSVLoader(file):
             uniquename = uniquename.replace(':', '')
             uniquename = uniquename.replace('&', '')
 
-            #Change so that it just loads each row, without condition
             if Movie.query.filter_by(uniquename=uniquename).first()  == None:
+
                 uniquename = Movie(uniquename=uniquename,
                                     name=row[0],
                                     year=row[1],
                                     video_link=row[2])
                 db.session.add(uniquename)
 
-
                 for t in range(3, len(row)):
-                    tagname = row[t]
-                    if Tag.query.filter_by(name=tagname).first()  == None:
-                        tagname = Tag(name=tagname)
+
+                    tagname = row[t].replace(' ', '')
+                    tagname = tagname.replace('&', '')
+                    tagname = tagname.replace('-', '')
+
+                    if Tag.query.filter_by(name= row[t]).first()  == None:
+                        tagname = Tag(name=row[t])
                         db.session.add(tagname)
-
-
-                    #Shouldn't need this else condition.. Although, might need to create
-                    #a modified tagname like mysterysuspense
-
                     else:
-                        tagname = Tag.query.filter_by(name=tagname).first()
+                        tagname = Tag.query.filter_by(name= row[t]).first()
 
                     uniquename.tags.append(tagname)
 
 
                 db.session.commit()
 
-    # Check Results
-    #
-    # for movie in Movie.query.all():
-    #     print(str(movie.movie_id) + ' ' + movie.uniquename + ' ' + movie.name + ' ' + str(movie.year) + ' ' + movie.video_link)
-    #     for t in movie.tags:
-    #         print(t.name)
-    # for tag in Tag.query.all():
-    #     print(tag.name)
-    #     for m in tag.movies:
-    #         print(m.name)
+    #Check Results
+
+    for movie in Movie.query.all():
+        print(str(movie.movie_id)+ '/')
+        print(movie.uniquename + '/')
+        print(movie.name + '/')
+        print(str(movie.year) + '/')
+        print(movie.video_link + '/')
+        for t in movie.tags:
+            print(t.name + '/')
+
+    for tag in Tag.query.all():
+        print(tag.name + '/')
+        for m in tag.movies:
+            print(m.name + '/')
