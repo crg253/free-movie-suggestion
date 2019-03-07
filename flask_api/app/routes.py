777cc82b21ad4f780d878bdc3fe05083dcf6b86a
crg253 <crg253@protonmail.com>
2019-03-06 22:53:40 -0800

    Added /movies json route and added json setting to config.

--- flask_api/app/routes.py
+++ flask_api/app/routes.py
@@ -1,6 +1,20 @@
+from flask import jsonify
 from app import app
+from app.models import Movie, Tag
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/movies')
+def movies():
+    movies = []
+    for movie in Movie.query.all():
+        movies.append({"id":movie.movie_id,
+                        "name":movie.name,
+                        "slug":movie.uniquename,
+                        "tags":[x.name for x in movie.tags],
+                        "video":movie.video_link,
+                        "year":movie.year})
+    return jsonify(movies)
