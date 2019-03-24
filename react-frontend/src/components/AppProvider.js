0c0b55cdbf8e7b7a494ccfed36baa045c46c4594
crg253 <crg253@protonmail.com>
2019-03-23 22:56:46 -0700

    Made some imporovements to the MovieList so that you can choose the genres.

--- react-frontend/src/components/AppProvider.js
+++ react-frontend/src/components/AppProvider.js
@@ -5,23 +5,40 @@ import axios from 'axios';
 class AppProvider extends Component {
   state = {
     Movies:[],
-    Genres:[]
-    }
+    Genres:[],
+    SelectedGenres:[],
+    SelectedMovies:[]
+  }
 
-    componentDidMount(){
-        axios.get('http://127.0.0.1:5000/movies')
-        .then(response=> {
-          this.setState({Movies: response.data})
-        })
-        axios.get('http://127.0.0.1:5000/genres')
-        .then(response=> {
-          this.setState({Genres: response.data})
-        })
-      }
+  //Each time you click on the genre, the movies that coorespond to that
+  //genre are added to SelectedMovies
+  //I will change this
+  //1. I will add the SelectedGenres back to state. This will be set to toggle
+  //2. I will then iterate through this and call this function
+  //3. Make sure there are no duplicates
+  //Q: That leaves the issue of how to handle other choices like..
+  //"all movies", movies by year range, etc
+  selectMoviesByGenre = (genre) =>{
+    const newSelections = [...this.state.Movies].filter(movie =>movie.tags.includes(genre));
+    newSelections.push.apply(newSelections, [...this.state.SelectedMovies]);
+    this.setState({SelectedMovies: newSelections});
+  }
+
+
+  componentDidMount(){
+      axios.get('http://127.0.0.1:5000/movies')
+      .then(response=> {
+        this.setState({Movies: response.data})
+      })
+      axios.get('http://127.0.0.1:5000/genres')
+      .then(response=> {
+        this.setState({Genres: response.data})
+      })
+  }
 
   render() {
     return (
-      <AppContext.Provider value={this.state}>
+      <AppContext.Provider value={{...this.state, selectMoviesByGenre: this.selectMoviesByGenre }}>
         {this.props.children}
       </AppContext.Provider>
     );
--- react-frontend/src/components/MovieList.js
+++ react-frontend/src/components/MovieList.js
@@ -5,23 +5,35 @@ import AppContext from "./AppContext";
 
 const MovieList = () => {
   return(
-        <AppProvider>
+
+    <AppProvider>
+
+      <div style={{display:"flex"}}>
         <AppContext.Consumer>
           {(context) =>  context.Genres.map(genre=>(
-            <div key={genre}>
+            <div key={genre} style={{marginRight:'10px'}}>
               <p>{genre}</p>
+
+              <AppContext.Consumer>
+                {(context) => <button onClick={()=>context.selectMoviesByGenre(genre)}></button>}
+              </AppContext.Consumer>
+
             </div>
           ))}
         </AppContext.Consumer>
-          <AppContext.Consumer>
-            {(context) =>  context.Movies.map(movie=>(
-              <div key={movie.id}>
-                <p>{movie.name}</p>
-              </div>
-            ))}
-          </AppContext.Consumer>
-        </AppProvider>
-    );
+      </div>
+
+      <h1>Selected Movies</h1>
+      <AppContext.Consumer>
+        {(context) =>  context.SelectedMovies.map(movie=>(
+          <div key={movie.id}>
+            <p>{movie.name} {movie.year} {movie.tags}</p>
+          </div>
+        ))}
+      </AppContext.Consumer>
+
+    </AppProvider>
+  );
 };
 
 export default MovieList;
