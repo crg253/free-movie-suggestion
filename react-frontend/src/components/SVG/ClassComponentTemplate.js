6bb9dd824fb77eebb30ced9f780f80b64b9bb8d1
crg253 <crg253@protonmail.com>
2019-04-05 18:46:38 -0700

    Made component for each section of the svg drawing. Added those components to HomePage.

---
+++ react-frontend/src/components/SVG/ClassComponentTemplate.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class ComponentName extends Component {
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default ComponentName;
---
+++ react-frontend/src/components/SVG/GradientsSVG.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+
+
+class GradientsSVG extends Component {
+
+  render() {
+    return (
+      <defs>
+        <linearGradient id="linear-gradient" x1="957.5" x2="957.5" y2="607" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#1b1464"/>
+          <stop offset="0.22" stopColor="#3b1970"/>
+          <stop offset="0.57" stopColor="#6b2181"/>
+          <stop offset="0.85" stopColor="#88258b"/>
+          <stop offset="1" stopColor="#93278f"/></linearGradient>
+        <linearGradient id="linear-gradient-2" x1="960.5" y1="601.3" x2="960.5" y2="757.52" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#603813"/>
+          <stop offset="0.07" stopColor="#573415"/>
+          <stop offset="0.32" stopColor="#3b291b"/>
+          <stop offset="0.54" stopColor="#29221f"/>
+          <stop offset="0.7" stopColor="#231f20"/></linearGradient>
+        <linearGradient id="linear-gradient-3" x1="959.5" y1="875.55" x2="959.5" y2="644.51" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#4d4d4d"/>
+          <stop offset="0.31"/></linearGradient>
+        <linearGradient id="linear-gradient-4" x1="1445.38" y1="133.04" x2="1445.38" y2="258.21" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#b25bb2" stopOpacity="0.8"/>
+          <stop offset="0.83" stopColor="#c16bc1" stopOpacity="0"/></linearGradient>
+        <linearGradient id="linear-gradient-5" x1="1447.52" y1="133.04" x2="1447.52" y2="258.21"/>
+        <linearGradient id="linear-gradient-6" x1="1438.7" y1="133.04" x2="1438.7" y2="258.21"/>
+        <linearGradient id="linear-gradient-7" x1="1411.32" y1="133.04" x2="1411.32" y2="258.21" />
+        <linearGradient id="linear-gradient-8" x1="1419.2" y1="133.04" x2="1419.2" y2="258.21"/>
+        <linearGradient id="linear-gradient-9" x1="1407.66" y1="133.04" x2="1407.66" y2="258.21"/>
+        <linearGradient id="linear-gradient-10" x1="1402.84" y1="133.04" x2="1402.84" y2="258.21"/>
+        <linearGradient id="linear-gradient-11" x1="1411.61" y1="133.04" x2="1411.61" y2="258.21"/>
+        <linearGradient id="linear-gradient-12" x1="1390.01" y1="133.04" x2="1390.01" y2="258.21"/>
+        <linearGradient id="linear-gradient-13" x1="1348.3" y1="133.04" x2="1348.3" y2="258.21"/>
+        <linearGradient id="linear-gradient-14" x1="1350.36" y1="133.04" x2="1350.36" y2="258.21"/>
+        <linearGradient id="linear-gradient-15" x1="1344.36" y1="133.04" x2="1344.36" y2="258.21"/>
+        <linearGradient id="linear-gradient-16" x1="1337.74" y1="133.04" x2="1337.74" y2="258.21"/>
+        <linearGradient id="linear-gradient-17" x1="1439.75" y1="133.04" x2="1439.75" y2="258.21"/>
+        <linearGradient id="linear-gradient-18" x1="1440.16" y1="133.04" x2="1440.16" y2="258.21"/>
+        <linearGradient id="linear-gradient-19" x1="1447.36" y1="133.04" x2="1447.36" y2="258.21"/>
+        <linearGradient id="linear-gradient-20" x1="1491.74" y1="133.04" x2="1491.74" y2="258.21"/>
+        <linearGradient id="linear-gradient-21" x1="1484.24" y1="133.04" x2="1484.24" y2="258.21"/>
+        <linearGradient id="linear-gradient-22" x1="1486.62" y1="133.04" x2="1486.62" y2="258.21"/>
+        <linearGradient id="linear-gradient-23" x1="1478.98" y1="133.04" x2="1478.98" y2="258.21"/>
+        <linearGradient id="linear-gradient-24" x1="1511.94" y1="133.04" x2="1511.94" y2="258.21"/>
+        <linearGradient id="linear-gradient-25" x1="1439.24" y1="133.04" x2="1439.24" y2="258.21"/>
+        <linearGradient id="linear-gradient-26" x1="1461.13" y1="133.04" x2="1461.13" y2="258.21"/>
+        <linearGradient id="linear-gradient-27" x1="1436.46" y1="133.04" x2="1436.46" y2="258.21"/>
+        <linearGradient id="linear-gradient-28" x1="1439.58" y1="133.04" x2="1439.58" y2="258.21"/>
+        <linearGradient id="linear-gradient-29" x1="1524.18" y1="133.04" x2="1524.18" y2="258.21" />
+        <linearGradient id="linear-gradient-30" x1="1521.52" y1="133.04" x2="1521.52" y2="258.21"/>
+        <linearGradient id="linear-gradient-31" x1="423.51" y1="375.38" x2="423.51" y2="794.29" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#99fe80" stopOpacity="0.7"/>
+          <stop offset="0.03" stopColor="#8fee78" stopOpacity="0.66"/>
+          <stop offset="0.14" stopColor="#6aaf58" stopOpacity="0.48"/>
+          <stop offset="0.26" stopColor="#49793d" stopOpacity="0.33"/>
+          <stop offset="0.39" stopColor="#2f4d27" stopOpacity="0.21"/>
+          <stop offset="0.52" stopColor="#1a2b16" stopOpacity="0.12"/>
+          <stop offset="0.66" stopColor="#0b130a" stopOpacity="0.05"/>
+          <stop offset="0.81" stopColor="#030502" stopOpacity="0.01"/>
+          <stop offset="1" stopOpacity="0"/></linearGradient>
+        <radialGradient id="radial-gradient" cx="1047.55" cy="291.52" r="212.98" gradientTransform="translate(0 87.46) scale(1 0.7)" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#fff" stopOpacity="0.6"/>
+          <stop offset="0.92" stopColor="#fff" stopOpacity="0"/></radialGradient>
+        <radialGradient id="radial-gradient-3" cx="1047.55" cy="292.52" r="212.98" gradientTransform="translate(0 87.76) scale(1 0.7)"/>
+        <radialGradient id="radial-gradient-7" cx="1047.55" cy="290.52" r="212.98" gradientTransform="translate(0 87.16) scale(1 0.7)"/>
+        <linearGradient id="linear-gradient-32" x1="1746.63" y1="150.33" x2="1780.91" y2="150.33" gradientUnits="userSpaceOnUse">
+          <stop offset="0" stopColor="#f0f4b4"/>
+          <stop offset="1" stopColor="#f0f4b4" stopOpacity="0.4"/></linearGradient>
+        <linearGradient id="linear-gradient-33" x1="1741.54" y1="56.73" x2="1779.86" y2="56.73"/>
+        <linearGradient id="linear-gradient-34" x1="1590.02" y1="46.26" x2="1651.83" y2="46.26"/>
+        <linearGradient id="linear-gradient-35" x1="1691.43" y1="27.68" x2="1716.03" y2="27.68"/>
+        <linearGradient id="linear-gradient-36" x1="1732.77" y1="140.43" x2="1774.83" y2="140.43"/>
+        <linearGradient id="linear-gradient-37" x1="1759.2" y1="54.48" x2="1774.97" y2="54.48"/>
+        <linearGradient id="linear-gradient-38" x1="1605.63" y1="193.83" x2="1623.03" y2="193.83"/>
+        <linearGradient id="linear-gradient-39" x1="1728.27" y1="189.03" x2="1756.23" y2="189.03"/>
+        <linearGradient id="linear-gradient-40" x1="1773.63" y1="69.86" x2="1778.43" y2="69.86" />
+        <linearGradient id="linear-gradient-41" x1="1589.37" y1="118.88" x2="1771.32" y2="118.88" />
+        <linearGradient id="linear-gradient-42" x1="1586.5" y1="109.23" x2="1615.83" y2="109.23"/>
+        <linearGradient id="linear-gradient-43" x1="1633.23" y1="36.07" x2="1737.63" y2="36.07"/>
+        <linearGradient id="linear-gradient-44" x1="1614.03" y1="193.32" x2="1676.43" y2="193.32" />
+      </defs>
+
+    );
+  }
+}
+
+export default GradientsSVG;
