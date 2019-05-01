fdfd85d0ea4592fbe2ae257be50fe1f389bd8e7f
crg253 <crg253@protonmail.com>
2019-05-01 11:21:23 -0700

    Movie genre dropdown menu mostly working. I'm a little confused about some of the css, so need to review that.

--- react-frontend/src/components/Genres.js
+++ react-frontend/src/components/Genres.js
@@ -18,45 +18,64 @@ class genres extends Component {
     return (
       <div>
 
-        <div id="listed-by">
+
+        <div
+          id="listed-by"
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent:"center"
+          }}>
+
           <h1 >{this.props.listby}</h1>
-          <button
+
+          <a href="javascript:void(0);"><div
             onClick={()=>this.changeGenreDisplay()}
-            style={{width:"10px", height:'10px', color:"white"}}></button>
+            style={{
+              marginLeft:"10px",
+              width:"20px",
+              height:"20px",
+              backgroundColor:"blue"}}>
+          </div></a>
+
+            <div
+              id="genre-link-list"
+              style={{
+                position:'absolute',
+                margin:"290px 0 0 0",
+                width:"75%",
+                display:this.state.displayGenres,
+                color:'black',
+                backgroundColor:"white"
+                }}>
+              {this.props.genres.map(genre=>(
+                  <a
+                    href="javascript:void(0);"
+                    onClick={()=>{this.props.selectBy(genre);this.changeGenreDisplay();}}
+                    style={{
+                      color:'black',
+                      display:"block"}}>{genre+" "} </a>
+              ))}
+              <a
+                href="javascript:void(0);"
+                onClick={()=>{this.props.selectBy("Saved");this.changeGenreDisplay();}}
+                style={{
+                  color:'black',
+                  display:"block"}}>Saved</a>
+              <a href="javascript:void(0);"
+                onClick={()=>{this.props.selectBy("All");this.changeGenreDisplay();}}
+                style={{
+                  color:'black',
+                  display:"block"}}>All Movies</a>
+              <a href="javascript:void(0);"
+                onClick={()=>{this.props.selectBy("User Suggestions");this.changeGenreDisplay();}}
+                style={{
+                  color:'black',
+                  display:"block"}}>USER SUGGESTIONS</a>
+              </div>
         </div>
 
-          <div
-            id="genre-link-list"
-            style={{
-              position:'absolute',
-              display:this.state.displayGenres,
-              color:'black',
-              backgroundColor:"white"}}>
-            {this.props.genres.map(genre=>(
-                <a
-                  href="javascript:void(0);"
-                  onClick={()=>{this.props.selectBy(genre);this.changeGenreDisplay();}}
-                  style={{
-                    color:'black',
-                    display:"block"}}>{genre+" "} </a>
-            ))}
-            <a
-              href="javascript:void(0);"
-              onClick={()=>{this.props.selectBy("Saved");this.changeGenreDisplay();}}
-              style={{
-                color:'black',
-                display:"block"}}>Saved</a>
-            <a href="javascript:void(0);"
-              onClick={()=>{this.props.selectBy("All");this.changeGenreDisplay();}}
-              style={{
-                color:'black',
-                display:"block"}}>All Movies</a>
-            <a href="javascript:void(0);"
-              onClick={()=>{this.props.selectBy("User Suggestions");this.changeGenreDisplay();}}
-              style={{
-                color:'black',
-                display:"block"}}>USER SUGGESTIONS</a>
-            </div>
+
 
         <div id="sort-by-wrapper">
           <p className='sort-selector'>sort by </p>
@@ -73,6 +92,8 @@ class genres extends Component {
             className='sort-selector'
             onClick = {()=>this.props.setSort("year")}>YEAR</p>
         </div>
+
+
       </div>
 
     );
