8aef3ef8017d9f1e0ed676c17620f0c009de737a
crg253 <crg253@protonmail.com>
2019-05-01 00:01:04 -0700

    Made genres dropdown button on TrailerPage.

--- react-frontend/src/components/Genres.js
+++ react-frontend/src/components/Genres.js
@@ -1,48 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const genres = (props) => {
+class genres extends Component {
+  state={
+    displayGenres:'none'
+  }
 
-  return(
-    <div>
-    <div style={{textAlign:"justify",textJustify:"inter-word"}}>
-      {props.genres.map(genre=>(
+  changeGenreDisplay = () =>{
+    if(this.state.displayGenres==="inline"){
+      this.setState({displayGenres:"none"})
+    }else{
+      this.setState({displayGenres:'inline'})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+        <div id="listed-by">
+          <h1 >{this.props.listby}</h1>
+          <button
+            onClick={()=>this.changeGenreDisplay()}
+            style={{width:"10px", height:'10px', color:"white"}}></button>
+        </div>
+
+          <div
+            id="genre-link-list"
+            style={{
+              position:'absolute',
+              display:this.state.displayGenres,
+              color:'black',
+              backgroundColor:"white"}}>
+            {this.props.genres.map(genre=>(
+                <a
+                  href="javascript:void(0);"
+                  onClick={()=>{this.props.selectBy(genre);this.changeGenreDisplay();}}
+                  style={{
+                    color:'black',
+                    display:"block"}}>{genre+" "} </a>
+            ))}
+            <a
+              href="javascript:void(0);"
+              onClick={()=>{this.props.selectBy("Saved");this.changeGenreDisplay();}}
+              style={{
+                color:'black',
+                display:"block"}}>Saved</a>
+            <a href="javascript:void(0);"
+              onClick={()=>{this.props.selectBy("All");this.changeGenreDisplay();}}
+              style={{
+                color:'black',
+                display:"block"}}>All Movies</a>
+            <a href="javascript:void(0);"
+              onClick={()=>{this.props.selectBy("User Suggestions");this.changeGenreDisplay();}}
+              style={{
+                color:'black',
+                display:"block"}}>USER SUGGESTIONS</a>
+            </div>
+
+        <div id="sort-by-wrapper">
+          <p className='sort-selector'>sort by </p>
           <p
-          style = {{color: props.inlistby(genre) ? 'Aqua': 'white'}}
-          className="genre-links"
-          onClick={()=>props.selectBy(genre)}>{genre+" "} </p>
-      ))}
-      <p
-        style = {{color: props.inlistby("Saved") ? 'Aqua': 'white'}}
-        className="genre-links"
-        onClick={()=>props.selectBy("Saved")}>Saved</p>
-      <p
-        style = {{color: props.inlistby("All") ? 'Aqua': 'white'}}
-        className="genre-links"
-        onClick={()=>props.selectBy("All")}>All Movies</p>
-      <p
-        style = {{color: props.inlistby("User Suggestions") ? 'Aqua': 'white'}}
-        className="genre-links"
-        onClick={()=>props.selectBy("User Suggestions")}>USER SUGGESTIONS</p>
+            style = {{
+              color: this.props.sortby==='name' ? 'LawnGreen': 'white'
+            }}
+            className='sort-selector'
+            onClick = {()=>this.props.setSort("name")}>TITLE</p>
+          <p
+            style = {{
+              color: this.props.sortby==='year' ? 'LawnGreen': 'white'
+            }}
+            className='sort-selector'
+            onClick = {()=>this.props.setSort("year")}>YEAR</p>
+        </div>
       </div>
 
-      <div id="sort-by-wrapper">
-      <p className='sort-selector'>sort by </p>
-      <p
-        style = {{
-          color: props.sortby==='name' ? 'LawnGreen': 'white'
-        }}
-        className='sort-selector'
-        onClick = {()=>props.setSort("name")}>TITLE</p>
-      <p
-        style = {{
-          color: props.sortby==='year' ? 'LawnGreen': 'white'
-        }}
-        className='sort-selector'
-        onClick = {()=>props.setSort("year")}>YEAR</p>
-    </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default genres;
