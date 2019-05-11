355daea0e56d9795aa3ed77b1546a7bdee9be52e
crg253 <crg253@protonmail.com>
2019-05-10 22:24:45 -0700

    Improved Menu.

--- react-frontend/src/components/Menu.js
+++ react-frontend/src/components/Menu.js
@@ -30,12 +30,27 @@ class Menu extends Component {
 
   render() {
 
-
     let signOutLink = ''
     if(this.props.user !==''){
       signOutLink= <div onClick={()=>{this.handleSignOut();this.props.setUser('')}}>
-                          <Link to={'/'}><h3>Sign Out</h3></Link>
-                        </div>
+                      <Link to={'/'}>
+                        <h4 className="menu-genre-links">Sign Out</h4>
+                      </Link>
+                    </div>
+    }
+
+    let menuGenreLinks = ''
+    const userPaths = ['/user', '/signin', '/adduser']
+    if (userPaths.includes(this.props.location.pathname)){
+      menuGenreLinks = this.props.genres.map(genre=>(
+                <div
+                  key={"menu" + genre}
+                  onClick={()=>{this.props.selectBy(genre);this.changeMenuDisplay();}}>
+                  <Link to={'/' + this.props.randomMovies[genre].slug}>
+                    <p className="menu-genre-links">{genre} </p>
+                  </Link>
+                </div>
+      ))
     }
 
 
@@ -43,32 +58,38 @@ class Menu extends Component {
       <div>
 
         <button
-          className="button-nostyle"
           id="menu-button"
+          className="button-nostyle"
           style={{display:this.state.displayButton}}
           onClick={()=>this.changeMenuDisplay()}></button>
 
-
-        <button
-          className="button-nostyle"
+        <div
           id="open-menu"
-          style={{display:this.state.displayMenu}}
-          onClick={()=>this.changeMenuDisplay()}>
+          style={{display:this.state.displayMenu}}>
 
-          <div id="menu-links">
-            <Link to={'/'}><h3>Home</h3></Link>
-            <Link to={'/user'}><h3>Contribute</h3></Link>
-            {signOutLink}
+          <button
+            id="close-menu-button"
+            className="button-nostyle"
+            onClick={()=>this.changeMenuDisplay()}>X</button>
 
-            {/*
-            {this.props.genres.map(genre=>(
-              <div onClick={()=>this.props.selectBy(genre)}>
-              <Link to={'/' + this.props.randomMovies[genre].slug}><p>{genre} </p></Link>
-              </div>//Javascript Comment
-            ))}
-            */}
+            <div id="menu-links">
+              <div onClick={()=>this.changeMenuDisplay()}>
+                <Link to={'/'}>
+                  <h4 className="menu-genre-links">Home</h4>
+                </Link>
+              </div>
+
+              <div onClick={()=>this.changeMenuDisplay()}>
+                <Link to={'/user'}>
+                  <h4 className="menu-genre-links">Contribute</h4>
+                </Link>
+              </div>
+              {signOutLink}
+              {menuGenreLinks}
+
+
           </div>
-        </button>
+        </div>
       </div>
     );
   }
