7b995bb0d688962770e5662266e926fa42f31acf
crg253 <crg253@protonmail.com>
2019-07-08 14:39:43 -0700

    Beautified all svg components.

--- react-frontend/src/components/SVG/SkyAndGroundSVG.js
+++ react-frontend/src/components/SVG/SkyAndGroundSVG.js
@@ -1,55 +1,124 @@
-import React, { Component } from 'react';
-
+import React, {Component} from "react";
 
 class SkyAndGroundSVG extends Component {
-
   render() {
     return (
       <g id="skyandground">
-
         <g id="sky">
-        <rect className="cls-1" width="1915" height="607"/>
+          <rect className="cls-1" width="1915" height="607" />
         </g>
 
-        <g id = "ground">
-        <rect className="cls-2" x="3" y="566" width="1915" height="161"/>
+        <g id="ground">
+          <rect className="cls-2" x="3" y="566" width="1915" height="161" />
         </g>
 
+        <polygon
+          className="cls-3"
+          points="1917 911.1 2 911.1 2 673 1917 678 1917 911.1"
+        />
+        <circle className="cls-4" cx="717" cy="22" r="7" />
+        <circle className="cls-4" cx="915" cy="758" r="7" />
+        <circle className="cls-4" cx="275" cy="138" r="7" />
+        <circle className="cls-4" cx="1135" cy="42" r="3.5" />
+        <circle className="cls-4" cx="880" cy="192" r="2.8" />
+        <circle className="cls-4" cx="1167" cy="31" r="6.16" />
+        <circle className="cls-4" cx="859" cy="79" r="3.5" />
+        <circle className="cls-4" cx="1360.24" cy="9.03" r="3.08" />
+        <circle className="cls-4" cx="1775.96" cy="210.19" r="2.8" />
+        <circle className="cls-4" cx="1836.97" cy="121.69" r="3.5" />
+        <circle className="cls-4" cx="871.96" cy="206.19" r="2.8" />
+        <circle className="cls-4" cx="955.47" cy="120.19" r="3.5" />
+        <circle className="cls-4" cx="86" cy="45" r="7" />
+        <circle className="cls-4" cx="75.13" cy="111.51" r="3.5" />
+        <circle className="cls-4" cx="267.84" cy="73.53" r="2.8" />
+        <circle className="cls-4" cx="301.33" cy="292.83" r="3.5" />
+        <circle className="cls-4" cx="380.95" cy="189.29" r="2.8" />
+        <circle className="cls-4" cx="261.15" cy="195.94" r="3.5" />
+        <circle className="cls-4" cx="922.34" cy="52.03" r="2.8" />
+        <circle className="cls-4" cx="864.83" cy="156.33" r="3.5" />
+        <circle className="cls-4" cx="990.45" cy="33.79" r="2.8" />
+        <circle className="cls-4" cx="1449.24" cy="118.17" r="7" />
+        <circle className="cls-4" cx="1746.26" cy="92.34" r="3.11" />
+        <circle className="cls-4" cx="1561.51" cy="50.96" r="3.5" />
+        <circle className="cls-4" cx="1464.01" cy="87.87" r="2.8" />
+        <circle className="cls-4" cx="1582.96" cy="193.64" r="3.5" />
+        <circle className="cls-4" cx="1619.89" cy="127.46" r="2.8" />
+        <circle className="cls-4" cx="1696.84" cy="197.17" r="2.8" />
+        <circle className="cls-4" cx="1667.31" cy="9.2" r="2.8" />
+        <circle className="cls-4" cx="1300.96" cy="168.19" r="2.8" />
+        <circle className="cls-4" cx="1285.26" cy="134.34" r="3.11" />
+        <circle className="cls-4" cx="1164.89" cy="175.46" r="2.8" />
+        <circle className="cls-4" cx="1087.97" cy="113.69" r="3.5" />
+        <circle className="cls-4" cx="1066.95" cy="76.29" r="2.8" />
+        <circle className="cls-4" cx="1814.73" cy="45.31" r="2.8" />
+        <circle className="cls-4" cx="1259.5" cy="32.5" r="3.5" />
+        <circle className="cls-4" cx="1891" cy="142.47" r="2.8" />
+        <circle className="cls-4" cx="1904.84" cy="23.39" r="3.5" />
+        <circle className="cls-4" cx="1569.04" cy="178.51" r="2.8" />
+        <circle className="cls-4" cx="1586.97" cy="307.26" r="3.5" />
+        <circle className="cls-4" cx="1605.32" cy="200.44" r="2.8" />
+        <circle className="cls-4" cx="1761.62" cy="29.8" r="4.2" />
+        <circle className="cls-4" cx="186" cy="62" r="5.42" />
+        <circle className="cls-4" cx="328" cy="119" r="3.5" />
+        <circle className="cls-4" cx="373.34" cy="103.03" r="2.8" />
+        <circle className="cls-4" cx="413.45" cy="122.79" r="2.8" />
+        <circle className="cls-4" cx="1756" cy="145" r="4.9" />
+        <circle className="cls-4" cx="1325" cy="40" r="7" />
+        <circle className="cls-4" cx="1314.13" cy="106.51" r="3.5" />
+        <circle className="cls-4" cx="1506.84" cy="68.53" r="2.8" />
+        <circle className="cls-4" cx="1519.95" cy="134.29" r="2.8" />
+        <circle className="cls-4" cx="1500.15" cy="190.94" r="3.5" />
+        <circle className="cls-4" cx="1425" cy="57" r="5.42" />
+        <circle className="cls-4" cx="1567" cy="114" r="3.5" />
+        <circle className="cls-4" cx="1630.34" cy="87.03" r="2.8" />
+        <circle className="cls-4" cx="1652.45" cy="117.79" r="2.8" />
+        <circle className="cls-4" cx="734" cy="204" r="2.16" />
+        <circle className="cls-4" cx="268" cy="15" r="2.8" />
+        <circle className="cls-4" cx="367.96" cy="53.19" r="2.8" />
+        <circle className="cls-4" cx="360.83" cy="5.33" r="3.5" />
+        <circle className="cls-4" cx="240" cy="91" r="4.9" />
+        <circle className="cls-4" cx="106" cy="221" r="2.8" />
+        <circle className="cls-4" cx="817.96" cy="47.19" r="2.8" />
+        <circle className="cls-4" cx="655.5" cy="33.5" r="3.5" />
+        <circle className="cls-4" cx="905" cy="115" r="4.9" />
+        <circle className="cls-4" cx="105" cy="30" r="2.8" />
+        <circle className="cls-4" cx="66.96" cy="64.19" r="2.8" />
+        <circle className="cls-4" cx="59.83" cy="14.33" r="3.5" />
+        <circle className="cls-4" cx="123" cy="94" r="4.9" />
+        <circle className="cls-4" cx="517" cy="29" r="2.8" />
+        <circle className="cls-4" cx="711.96" cy="90.19" r="2.8" />
+        <circle className="cls-4" cx="1226.83" cy="62.33" r="3.5" />
+        <circle className="cls-4" cx="1116" cy="130" r="4.9" />
+        <circle className="cls-4" cx="1375.5" cy="117.5" r="3.5" />
+        <circle className="cls-4" cx="1045.24" cy="216.17" r="7" />
+        <circle className="cls-4" cx="1167.51" cy="49.96" r="3.5" />
+        <circle className="cls-4" cx="145.01" cy="224.87" r="2.8" />
+        <circle className="cls-4" cx="1188.96" cy="192.64" r="3.5" />
+        <circle className="cls-4" cx="1225.89" cy="126.46" r="2.8" />
+        <circle className="cls-4" cx="1082.84" cy="156.17" r="3.5" />
+        <circle className="cls-4" cx="1273.31" cy="22.2" r="2.8" />
+        <circle className="cls-4" cx="941.96" cy="187.19" r="2.8" />
+        <circle className="cls-4" cx="891.26" cy="133.34" r="3.11" />
+        <circle className="cls-4" cx="865.5" cy="31.5" r="3.5" />
+        <circle className="cls-4" cx="1839.04" cy="17.51" r="1.68" />
+        <circle className="cls-4" cx="1211.32" cy="199.44" r="2.8" />
+        <circle className="cls-4" cx="931" cy="39" r="7" />
+        <circle className="cls-4" cx="1582.5" cy="19.5" r="3.5" />
+        <circle className="cls-4" cx="1112.84" cy="67.53" r="2.8" />
+        <circle className="cls-4" cx="1225.95" cy="183.29" r="2.8" />
+        <circle className="cls-4" cx="1106.15" cy="189.94" r="3.5" />
+        <circle className="cls-4" cx="1031" cy="56" r="5.42" />
+        <circle className="cls-4" cx="1173" cy="113" r="3.5" />
+        <circle className="cls-4" cx="1236.34" cy="86.03" r="2.8" />
+        <circle className="cls-4" cx="1438.45" cy="35.79" r="3.42" />
+        <circle className="cls-4" cx="832.83" cy="61.33" r="3.5" />
+        <circle className="cls-4" cx="991.5" cy="136.5" r="3.5" />
 
-        <polygon className="cls-3" points="1917 911.1 2 911.1 2 673 1917 678 1917 911.1"/>
-        <circle className="cls-4" cx="717" cy="22" r="7"/>
-        <circle className="cls-4" cx="915" cy="758" r="7"/>
-        <circle className="cls-4" cx="275" cy="138" r="7"/>
-        <circle className="cls-4" cx="1135" cy="42" r="3.5"/>
-        <circle className="cls-4" cx="880" cy="192" r="2.8"/>
-        <circle className="cls-4" cx="1167" cy="31" r="6.16"/>
-        <circle className="cls-4" cx="859" cy="79" r="3.5"/>
-        <circle className="cls-4" cx="1360.24" cy="9.03" r="3.08"/>
-        <circle className="cls-4" cx="1775.96" cy="210.19" r="2.8"/>
-        <circle className="cls-4" cx="1836.97" cy="121.69" r="3.5"/>
-        <circle className="cls-4" cx="871.96" cy="206.19" r="2.8"/>
-        <circle className="cls-4" cx="955.47" cy="120.19" r="3.5"/>
-        <circle className="cls-4" cx="86" cy="45" r="7"/>
-        <circle className="cls-4" cx="75.13" cy="111.51" r="3.5"/>
-        <circle className="cls-4" cx="267.84" cy="73.53" r="2.8"/>
-        <circle className="cls-4" cx="301.33" cy="292.83" r="3.5"/>
-        <circle className="cls-4" cx="380.95" cy="189.29" r="2.8"/>
-        <circle className="cls-4" cx="261.15" cy="195.94" r="3.5"/>
-        <circle className="cls-4" cx="922.34" cy="52.03" r="2.8"/>
-        <circle className="cls-4" cx="864.83" cy="156.33" r="3.5"/>
-        <circle className="cls-4" cx="990.45" cy="33.79" r="2.8"/>
-        <circle className="cls-4" cx="1449.24" cy="118.17" r="7"/>
-        <circle className="cls-4" cx="1746.26" cy="92.34" r="3.11"/>
-        <circle className="cls-4" cx="1561.51" cy="50.96" r="3.5"/>
-        <circle className="cls-4" cx="1464.01" cy="87.87" r="2.8"/>
-        <circle className="cls-4" cx="1582.96" cy="193.64" r="3.5"/><circle className="cls-4" cx="1619.89" cy="127.46" r="2.8"/><circle className="cls-4" cx="1696.84" cy="197.17" r="2.8"/><circle className="cls-4" cx="1667.31" cy="9.2" r="2.8"/><circle className="cls-4" cx="1300.96" cy="168.19" r="2.8"/><circle className="cls-4" cx="1285.26" cy="134.34" r="3.11"/><circle className="cls-4" cx="1164.89" cy="175.46" r="2.8"/><circle className="cls-4" cx="1087.97" cy="113.69" r="3.5"/><circle className="cls-4" cx="1066.95" cy="76.29" r="2.8"/><circle className="cls-4" cx="1814.73" cy="45.31" r="2.8"/><circle className="cls-4" cx="1259.5" cy="32.5" r="3.5"/><circle className="cls-4" cx="1891" cy="142.47" r="2.8"/><circle className="cls-4" cx="1904.84" cy="23.39" r="3.5"/><circle className="cls-4" cx="1569.04" cy="178.51" r="2.8"/><circle className="cls-4" cx="1586.97" cy="307.26" r="3.5"/><circle className="cls-4" cx="1605.32" cy="200.44" r="2.8"/><circle className="cls-4" cx="1761.62" cy="29.8" r="4.2"/><circle className="cls-4" cx="186" cy="62" r="5.42"/><circle className="cls-4" cx="328" cy="119" r="3.5"/><circle className="cls-4" cx="373.34" cy="103.03" r="2.8"/><circle className="cls-4" cx="413.45" cy="122.79" r="2.8"/><circle className="cls-4" cx="1756" cy="145" r="4.9"/><circle className="cls-4" cx="1325" cy="40" r="7"/><circle className="cls-4" cx="1314.13" cy="106.51" r="3.5"/><circle className="cls-4" cx="1506.84" cy="68.53" r="2.8"/><circle className="cls-4" cx="1519.95" cy="134.29" r="2.8"/><circle className="cls-4" cx="1500.15" cy="190.94" r="3.5"/><circle className="cls-4" cx="1425" cy="57" r="5.42"/><circle className="cls-4" cx="1567" cy="114" r="3.5"/><circle className="cls-4" cx="1630.34" cy="87.03" r="2.8"/><circle className="cls-4" cx="1652.45" cy="117.79" r="2.8"/><circle className="cls-4" cx="734" cy="204" r="2.16"/><circle className="cls-4" cx="268" cy="15" r="2.8"/><circle className="cls-4" cx="367.96" cy="53.19" r="2.8"/><circle className="cls-4" cx="360.83" cy="5.33" r="3.5"/><circle className="cls-4" cx="240" cy="91" r="4.9"/><circle className="cls-4" cx="106" cy="221" r="2.8"/><circle className="cls-4" cx="817.96" cy="47.19" r="2.8"/><circle className="cls-4" cx="655.5" cy="33.5" r="3.5"/><circle className="cls-4" cx="905" cy="115" r="4.9"/><circle className="cls-4" cx="105" cy="30" r="2.8"/><circle className="cls-4" cx="66.96" cy="64.19" r="2.8"/><circle className="cls-4" cx="59.83" cy="14.33" r="3.5"/><circle className="cls-4" cx="123" cy="94" r="4.9"/><circle className="cls-4" cx="517" cy="29" r="2.8"/><circle className="cls-4" cx="711.96" cy="90.19" r="2.8"/><circle className="cls-4" cx="1226.83" cy="62.33" r="3.5"/><circle className="cls-4" cx="1116" cy="130" r="4.9"/><circle className="cls-4" cx="1375.5" cy="117.5" r="3.5"/><circle className="cls-4" cx="1045.24" cy="216.17" r="7"/><circle className="cls-4" cx="1167.51" cy="49.96" r="3.5"/><circle className="cls-4" cx="145.01" cy="224.87" r="2.8"/><circle className="cls-4" cx="1188.96" cy="192.64" r="3.5"/><circle className="cls-4" cx="1225.89" cy="126.46" r="2.8"/><circle className="cls-4" cx="1082.84" cy="156.17" r="3.5"/><circle className="cls-4" cx="1273.31" cy="22.2" r="2.8"/><circle className="cls-4" cx="941.96" cy="187.19" r="2.8"/><circle className="cls-4" cx="891.26" cy="133.34" r="3.11"/><circle className="cls-4" cx="865.5" cy="31.5" r="3.5"/><circle className="cls-4" cx="1839.04" cy="17.51" r="1.68"/><circle className="cls-4" cx="1211.32" cy="199.44" r="2.8"/><circle className="cls-4" cx="931" cy="39" r="7"/><circle className="cls-4" cx="1582.5" cy="19.5" r="3.5"/><circle className="cls-4" cx="1112.84" cy="67.53" r="2.8"/><circle className="cls-4" cx="1225.95" cy="183.29" r="2.8"/><circle className="cls-4" cx="1106.15" cy="189.94" r="3.5"/><circle className="cls-4" cx="1031" cy="56" r="5.42"/><circle className="cls-4" cx="1173" cy="113" r="3.5"/><circle className="cls-4" cx="1236.34" cy="86.03" r="2.8"/><circle className="cls-4" cx="1438.45" cy="35.79" r="3.42"/><circle className="cls-4" cx="832.83" cy="61.33" r="3.5"/><circle className="cls-4" cx="991.5" cy="136.5" r="3.5"/>
+        <rect className="cls-5" x="1571" y="457" width="175" height="106" />
 
-        <rect className="cls-5" x="1571" y="457" width="175" height="106"/>
-
-        <polygon points="838 432 807 157 1279 141 1303 451 838 432"/>
-        <circle className="cls-4" cx="17" cy="38" r="2.1"/>
-
-        </g>
+        <polygon points="838 432 807 157 1279 141 1303 451 838 432" />
+        <circle className="cls-4" cx="17" cy="38" r="2.1" />
+      </g>
     );
   }
 }
