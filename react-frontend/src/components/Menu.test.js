887895e8ca511ee1853218e1f96944c32f6895d1
crg253 <crg253@protonmail.com>
2019-07-27 17:39:39 -0700

    Update react Menu.test.js to test for changes to About and Contact. Renamed python tests.py to unittest_tests.py.

--- react-frontend/src/components/Menu.test.js
+++ react-frontend/src/components/Menu.test.js
@@ -1,93 +1,67 @@
-import Menu from './Menu';
+import Menu from "./Menu";
 
-
-function createTestPropsNoUser(props){
-  return{
-    user:'',
-    ...props,
+function createTestPropsNoUser(props) {
+  return {
+    user: "",
+    ...props
   };
 }
 
-function createTestPropsUser(props){
-  return{
-    user:'monkey',
-    ...props,
+function createTestPropsUser(props) {
+  return {
+    user: "monkey",
+    ...props
   };
 }
 
-let propsNoUser, noUserWrapper, propsUser, userWrapper ;
+let propsNoUser, noUserWrapper, propsUser, userWrapper;
 
-beforeEach(() =>{
+beforeEach(() => {
   propsNoUser = createTestPropsNoUser();
-  noUserWrapper = shallow(<Menu {...propsNoUser}/>);
+  noUserWrapper = shallow(<Menu {...propsNoUser} />);
   propsUser = createTestPropsUser();
   userWrapper = shallow(<Menu {...propsUser} />);
 });
 
-describe('<Menu />',() =>{
-  it('renders with no props', () =>{
+describe("<Menu />", () => {
+  it("renders with no props", () => {
     noUserWrapper;
     expect(
-      noUserWrapper.containsMatchingElement(
-            <h2>Sign In</h2>
-      )
-    ).toBeTruthy()
+      noUserWrapper.containsMatchingElement(<h2>Sign In</h2>)
+    ).toBeTruthy();
     expect(
-      noUserWrapper.containsMatchingElement(
-            <h2>Recommend</h2>
-      )
-    ).toBeTruthy()
+      noUserWrapper.containsMatchingElement(<h2>Recommend</h2>)
+    ).toBeTruthy();
     expect(
-      noUserWrapper.containsMatchingElement(
-            <h2>User Suggestions</h2>
-      )
-    ).toBeTruthy()
+      noUserWrapper.containsMatchingElement(<h2>User Suggestions</h2>)
+    ).toBeTruthy();
+    expect(noUserWrapper.containsMatchingElement(<h2>About</h2>)).toBeTruthy();
     expect(
-      noUserWrapper.containsMatchingElement(
-            <h2>About/Contact</h2>
-      )
-    ).toBeTruthy()
+      noUserWrapper.containsMatchingElement(<h2>Contact</h2>)
+    ).toBeTruthy();
   });
 });
 
-describe('<Menu />',() =>{
-  it('renders with user name passed as props', () =>{
+describe("<Menu />", () => {
+  it("renders with user name passed as props", () => {
     userWrapper;
     expect(
-      userWrapper.containsMatchingElement(
-            <h2>monkey's Movies</h2>
-      )
-    ).toBeTruthy()
+      userWrapper.containsMatchingElement(<h2>monkey's Movies</h2>)
+    ).toBeTruthy();
     expect(
-      userWrapper.containsMatchingElement(
-            <h2>Recommend</h2>
-      )
-    ).toBeTruthy()
+      userWrapper.containsMatchingElement(<h2>Recommend</h2>)
+    ).toBeTruthy();
     expect(
-      userWrapper.containsMatchingElement(
-            <h2>User Suggestions</h2>
-      )
-    ).toBeTruthy()
+      userWrapper.containsMatchingElement(<h2>User Suggestions</h2>)
+    ).toBeTruthy();
+    expect(userWrapper.containsMatchingElement(<h2>About</h2>)).toBeTruthy();
+    expect(userWrapper.containsMatchingElement(<h2>Contact</h2>)).toBeTruthy();
     expect(
-      userWrapper.containsMatchingElement(
-            <h2>About/Contact</h2>
-      )
-    ).toBeTruthy()
+      userWrapper.containsMatchingElement(<p>edit account</p>)
+    ).toBeTruthy();
     expect(
-      userWrapper.containsMatchingElement(
-            <p>edit account</p>
-      )
-    ).toBeTruthy()
-    expect(
-      userWrapper.containsMatchingElement(
-            <p>delete account</p>
-      )
-    ).toBeTruthy()
-    expect(
-      userWrapper.containsMatchingElement(
-            <p>sign out</p>
-      )
-    ).toBeTruthy()
-
+      userWrapper.containsMatchingElement(<p>delete account</p>)
+    ).toBeTruthy();
+    expect(userWrapper.containsMatchingElement(<p>sign out</p>)).toBeTruthy();
   });
 });
